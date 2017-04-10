89c1da41a141eb6094aca98ab8d3ed17c42990a6
maojun.zy <shakazxx@gmail.com>
2017-04-10 22:07:07 +0800

    Enhance proxy check and kill existing dead process

--- script/reverseProxy.sh
+++ script/reverseProxy.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-sshStatus=`ps -ef|grep "104.207.153.248"|grep "8122"`
-if [ -z "$sshStatus" ]; then
-	ssh -fCNR 8122:localhost:22 shakazxx@104.207.153.248 & >/dev/null 2>&1
-	echo "Reverse proxy creatd success!"
+check_connection() {
+	conn=`nc -w 1 104.207.153.248 8022|grep SSH`
+	if [ -z "$conn" ]; then
+		return 1
+	else
+		return 0
+	fi
+}
+
+res=check_connection
+if "$res"; then
+	echo "Reverse proxy already existed!"
+	exit 0
 else
-	echo "Reverse proxy already built!"
+	echo "Reverse proxy not existed!"
 fi
+
+for i in {1..3}
+do
+	ps -ef|grep "104.207.153.248"|grep "8122"|awk '{print $2}'|xargs kill -9
+	ssh -fCNR 8122:localhost:22 shakazxx@104.207.153.248 & >/dev/null 2>&1
+	sleep 5
+	res=check_connection
+	if "$res"; then
+                echo "Reverse proxy created success!"
+		exit 0
+	else
+		echo "Reverse proxy created fail!"
+	fi
+done
+exit 0
