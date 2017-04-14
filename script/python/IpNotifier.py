974208be89a1d68437f38d100c2c935a8f5be813
AaronGeist <shakazxx@gmail.com>
2017-04-14 22:02:25 +0800

    Check if IP is changed then nofity

--- script/python/IpNotifier.py
+++ script/python/IpNotifier.py
@@ -2,7 +2,21 @@ import os
 
 from library.EmailSender import EmailSender
 
-output = os.popen("curl ip.gs | awk '{print $2}' | awk -F'：' '{print $2}'")
-ip = output.read().strip()
+FILE = "/var/tmp/ip.txt"
+oldIp = ""
 
-EmailSender.quickSend(u"最新IP: " + str(ip), "")
+if os.path.exists(FILE):
+    with open(FILE, "r") as fp:
+        oldIp = fp.readline().strip()
+
+output = os.popen("curl ip.gs | awk '{print $2}' | awk -F'：' '{print $2}'")
+newIp = output.read().strip()
+
+if oldIp != newIp:
+    with open(FILE, "w") as fp:
+        fp.writelines(newIp)
+    print("old[%s], new[%s]" % (oldIp, newIp))
+    EmailSender.quickSend(u"最新IP: " + str(newIp), "")
+else:
+    print("same ip[%s]" % oldIp)
+
