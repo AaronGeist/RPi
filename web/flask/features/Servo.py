271ea868619b91e573e48c000713a9c5e0bfe0f1
AaronGeist <shakazxx@gmail.com>
2017-04-22 11:56:24 +0800

    Support vertical control for servo

--- web/flask/features/Servo.py
+++ web/flask/features/Servo.py
@@ -1,42 +1,77 @@
 import RPi.GPIO as GPIO
 import time
+from Monitor import Database
 
 
 class Servo:
-    duty_cycle = 0
-    step = 10
-    pin = None
+    hPin = None
+    vPin = None
+
+    H_DB_KEY = "horizontal_ducy_cycle"
+    V_DB_KEY = "vertical_ducy_cycle"
 
     @staticmethod
     def initialize():
-        if Servo.pin is None:
-            Servo.duty_cycle = 7.5
+        if Servo.hPin is None and Servo.vPin is None:
+            if Database().get(Servo.H_DB_KEY) is None:
+                Database().set(Servo.H_DB_KEY, 7.5)
+            if Database().get(Servo.V_DB_KEY) is None:
+                Database().set(Servo.V_DB_KEY, 7.5)
+
+            ## horizontal servo
             GPIO.setmode(GPIO.BCM)
             GPIO.setup(17, GPIO.OUT, initial=False)
-            Servo.pin = GPIO.PWM(17, 50)  # 50HZ
-            Servo.pin.start(0)
+            Servo.hPin = GPIO.PWM(17, 50)  # 50HZ
+            Servo.hPin.start(0)
+
+            ## vertical servo
+            GPIO.setup(19, GPIO.OUT, initial=False)
+            Servo.vPin = GPIO.PWM(19, 50)  # 50HZ
+            Servo.vPin.start(0)
+
+    @staticmethod
+    def control(vDelta=0, hDelta=0):
+        Servo.initialize()
+        if vDelta != 0:
+            duty_cycle = float(Database().get(Servo.V_DB_KEY))
+            duty_cycle += vDelta
+            duty_cycle = max(duty_cycle, 2.5)
+            duty_cycle = min(duty_cycle, 12.5)
+            print(duty_cycle)
+            Database().set(Servo.V_DB_KEY, duty_cycle)
+            Servo.vPin.ChangeDutyCycle(duty_cycle)
+            time.sleep(0.02)
+            Servo.vPin.ChangeDutyCycle(0)
+            time.sleep(0.05)
+
+        if hDelta != 0:
+            duty_cycle = float(Database().get(Servo.H_DB_KEY))
+            duty_cycle += hDelta
+            duty_cycle = max(duty_cycle, 2.5)
+            duty_cycle = min(duty_cycle, 12.5)
+            print(duty_cycle)
+            Database().set(Servo.H_DB_KEY, duty_cycle)
+            Servo.hPin.ChangeDutyCycle(duty_cycle)
+            time.sleep(0.02)
+            Servo.hPin.ChangeDutyCycle(0)
+            time.sleep(0.05)
 
     @staticmethod
     def left():
-        Servo.initialize()
-        Servo.duty_cycle += 0.5
-        Servo.duty_cycle = min(Servo.duty_cycle, 12.5)
-        print(Servo.duty_cycle)
-        Servo.pin.ChangeDutyCycle(Servo.duty_cycle)
-        time.sleep(0.02)
-        Servo.pin.ChangeDutyCycle(0)
-        time.sleep(0.05)
+        Servo.control(hDelta=-0.5)
 
     @staticmethod
     def right():
-        Servo.initialize()
-        Servo.duty_cycle -= 0.5
-        Servo.duty_cycle = max(Servo.duty_cycle, 2.5)
-        print(Servo.duty_cycle)
-        Servo.pin.ChangeDutyCycle(Servo.duty_cycle)
-        time.sleep(0.02)
-        Servo.pin.ChangeDutyCycle(0)
-        time.sleep(0.05)
+        Servo.control(hDelta=0.5)
+
+    @staticmethod
+    def up():
+        Servo.control(vDelta=0.5)
+
+    @staticmethod
+    def down():
+        Servo.control(vDelta=-0.5)
+
 
 if __name__ == "__main__":
 	Servo.left();
