a1e9f10891dafa0bd10bcf139def445b9d03ba94
maojun.zy <maojun.zy@alipay.com>
2019-07-06 16:56:17 +0800

    Fix tab space mix issue.

--- web/flask/index.py
+++ web/flask/index.py
@@ -6,46 +6,56 @@ import time
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-     return 'Hello World'
+    return 'Hello World'
+
 
 @app.route('/name/<username>')
 def hellouser(username):
-     return 'Hello %s' % username
+    return 'Hello %s' % username
+
 
 @app.route('/monitor/', methods=['GET', 'POST'])
 def monitor():
     return render_template('monitor/index.html')
 
+
 @app.route('/monitor/cpu/temperature', methods=['GET', 'POST'])
 def monitor_cpu_temperature():
     return jsonify(CpuTemperature().history())
+
 
 @app.route('/monitor/cpu/temperature/1', methods=['GET', 'POST'])
 def monitor_cpu_temperature_single():
     return jsonify(CpuTemperature().latest())
 
+
 @app.route('/monitor/memory/usage', methods=['GET', 'POST'])
 def monitor_memory_usage():
     return jsonify(Memory().history())
+
 
 @app.route('/monitor/memory/usage/1', methods=['GET', 'POST'])
 def monitor_memory_usage_single():
     return jsonify(Memory().latest())
 
+
 def gen(camera):
     while True:
-        #print("take snapshot")
+        # print("take snapshot")
         frame = camera.snapshot()
-	yield (b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         # sleep to make sure frame generation speed could match the show speed
         time.sleep(0.05)
+
 
 @app.route('/monitor/camera/', methods=['GET', 'POST'])
 def monitor_camera():
     return render_template('monitor/camera.html')
+
 
 @app.route('/monitor/camera/servo/', methods=['POST'])
 def monitor_camera_servo():
@@ -56,9 +66,11 @@ def monitor_camera_servo():
         Servo.right()
     return ""
 
+
 @app.route('/monitor/camera/face/', methods=['GET'])
 def monitor_camera_face():
     return str(Camera.is_enable_face_detect())
+
 
 @app.route('/monitor/camera/face/set/', methods=['POST'])
 def monitor_camera_face_set():
@@ -69,9 +81,11 @@ def monitor_camera_face_set():
         Camera.disable_face_detect()
     return ""
 
+
 @app.route('/monitor/camera/brightness/get/', methods=['GET'])
 def monitor_camera_brightness_get():
     return str(Camera.getBrightness())
+
 
 @app.route('/monitor/camera/brightness/set/', methods=['POST'])
 def monitor_camera_brightness_set():
@@ -79,10 +93,12 @@ def monitor_camera_brightness_set():
     Camera.setBrightness(delta)
     return ""
 
+
 @app.route('/camera/', methods=['GET', 'POST'])
 def camera():
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 if __name__ == '__main__':
-       app.run(host='0.0.0.0', port=8888, debug=True)
+    app.run(host='0.0.0.0', port=8888, debug=True)
